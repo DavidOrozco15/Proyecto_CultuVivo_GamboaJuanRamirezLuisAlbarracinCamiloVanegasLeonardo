57c73c8f1cdc91f8778e967b9a55e6b006f56f89
camiloaav2607-maker <226595475+camiloaav2607-maker@users.noreply.github.com>
2025-10-23 02:03:50 -0500

    feat(asistentes): se creo la funcion mostrar_asistentes que muestra la lista de asistentes almacenados en el json y se agrego una funcion de menu de prueba para verificar que el codigo funcione correctamente

--- asistentes.py
+++ asistentes.py
@@ -77,4 +77,36 @@ def registrar_asistente():
     guardar_asistentes(asistentes)
     print("Registro exitoso, su estado es: En espera.")
 
-        
+def mostrar_asistentes():
+    asistentes = cargar_asistentes()
+    if not asistentes:
+        print("No hay asistentes registrados.")
+        return
+    
+    print("===LISTA DE ASISTENTES===")
+    for idx, asistente in enumerate(asistentes, start=1):
+        print(f"\nAsistente #{idx}")
+        print(f"Identificación: {asistente['identificacion']}")
+        print(f"Nombre: {asistente['nombre']}")
+        print(f"Correo: {asistente['correo']}")
+        print(f"Tipo de Boleto: {asistente['tipo_boleto']}")
+        print(f"Estado: {asistente['estado']}")    
+
+if __name__ == "__main__":
+    while True:
+        print("\n--- Menú de Registro de Asistentes ---")
+        print("1. Registrar asistente")
+        print("2. Mostrar asistentes")
+        print("3. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            registrar_asistente()
+        elif opcion == "2":
+            mostrar_asistentes()
+        elif opcion == "3":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción inválida. Intente nuevamente.")
